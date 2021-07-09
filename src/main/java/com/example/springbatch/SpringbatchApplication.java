73478904398ceb92f1f05dba0215457dca60d8d2
danwook <k.danwoo91@ibksystem.co.kr>
2021-07-09 19:09:17 +0900

    스프링 배치 basic 구현.
    Job -> Step -> tasklet
    (청크지향 아님)

--- src/main/java/com/example/springbatch/SpringbatchApplication.java
+++ src/main/java/com/example/springbatch/SpringbatchApplication.java
@@ -1,8 +1,15 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 위 사진과 같이 스프링 배치의 기능을 활성화하기 위해서 설정과 관련된 어노테이션(@EnableBatchProcessing)을 사용해줍니다.
+ * */
+@EnableScheduling //스케줄러 기능 활성화
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringbatchApplication {
 
---
+++ src/main/java/com/example/springbatch/tasklets/TutorialTasklet.java
@@ -0,0 +1,19 @@
+package com.example.springbatch.tasklets;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+@Slf4j
+public class TutorialTasklet implements Tasklet {
+    /**
+     * Tasklet -> Spring batch Core에서 제공하는 Step안의 인터페이스
+     * */
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        log.debug("executed tasklet !!");
+        return RepeatStatus.FINISHED;
+    }
+}
